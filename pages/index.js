68c1cf356b3f4b8d8a049985fe6ade09e96fba67
MugiSus <mmgiss256@gmail.com>
2022-08-09 10:14:35 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -76,7 +76,7 @@ export default function Home() {
             const date = new Date(Date.now() + localTimeDifference);
             document.getElementsByClassName(styles.code)[2].textContent = format(Date.now(), 'yyyy/MM/dd HH:mm:ss.SSS');
             document.getElementsByClassName(styles.code)[3].textContent = format(date, 'yyyy/MM/dd HH:mm:ss.SSS');
-            document.getElementsByClassName(styles.code)[4].textContent = (localTimeDifference > 0 ? "+" : "-") + format(Math.abs(localTimeDifference) + timeZoneOffset, 'HH:mm:ss.SSS');
+            document.getElementsByClassName(styles.code)[4].textContent = (localTimeDifference >= 0 ? "+" : "-") + format(Math.abs(localTimeDifference) + timeZoneOffset, 'HH:mm:ss.SSS');
             requestAnimationFrame(updateTimeText);
         }
         updateTimeText();
