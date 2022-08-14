7bbfe7e3845b39eb0ce45c83c256c2ab5f1ad595
MugiSus <mmgiss256@gmail.com>
2022-08-14 21:57:16 +0900

    commit

--- pages/index.js
+++ pages/index.js
@@ -102,7 +102,7 @@ export default function Home() {
         // init
         const camera = new THREE.PerspectiveCamera( 90, window.innerWidth / window.innerHeight, 0.01, 10 );
         camera.position.z = 1;
-        
+
         const scene = new THREE.Scene();
         const renderer = new THREE.WebGLRenderer({
             antialias: true,
@@ -116,7 +116,7 @@ export default function Home() {
             '/icosphere_earth.glb',
             (gltf) => {
                 const model = gltf.scene.children[0];
-                model.scale.set(0.4, 0.4, 0.4);
+                model.scale.set(0.5, 0.5, 0.5);
                 model.material = new THREE.MeshNormalMaterial({
                     wireframe: true,
                 });
