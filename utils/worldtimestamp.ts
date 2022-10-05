428cbc18c6df975dbe1f189e5063aee3869711f1
MugiSus <mmgiss256@gmail.com>
2022-10-05 18:11:07 +0900

    Update worldtimestamp.ts

--- utils/worldtimestamp.ts
+++ utils/worldtimestamp.ts
@@ -4,15 +4,15 @@ const getWorldTimestamp = async (timezone: string = 'Etc/GMT'): Promise<number> 
     const res: Response = await fetch(`https://worldtimeapi.org/api/timezone/${timezone}`);
     const json: any = await res.json();
 
-    const status: number = res.status;
+    const status = res.status;
 
     if (status >= 400)
         throw Error(json.message);
 
     const datetime: string = json.datetime;
 
-    const nanoseconds: number = +json.datetime.match(/\.\d{3}(\d*?)\+/)[1];
-    const serverTimestamp: number = new Date(datetime).getTime() + nanoseconds / 1000;
+    const nanoseconds = +json.datetime.match(/\.\d{3}(\d*?)\+/)[1];
+    const serverTimestamp = new Date(datetime).getTime() + nanoseconds / 1000;
 
     return serverTimestamp;
 }
