611dbb73db6de9ab8e90a3be936d6029640ce7fb
MugiSus <mmgiss256@gmail.com>
2022-10-13 12:25:50 +0900

    update worldtimestamp.ts

--- utils/worldtimestamp.ts
+++ utils/worldtimestamp.ts
@@ -1,20 +1,49 @@
 import fetch from 'isomorphic-unfetch';
 
-const getWorldTimestamp = async (timezone: string = 'Etc/GMT'): Promise<number> => {
+interface DateTimeJsonResponse {
+    abbreviation: string;
+    client_ip: string;
+    datetime: string;
+    day_of_week: number;
+    day_of_year: number;
+    dst: boolean;
+    dst_from: string;
+    dst_offset: number;
+    dst_until: string;
+    raw_offset: number;
+    timezone: string;
+    unixtime: number;
+    utc_datetime: string;
+    utc_offset: string;
+    week_number: number;
+}
+
+interface WorldTimestamp {
+    response: DateTimeJsonResponse;
+    timestamp: number;
+    utcTimestamp: number;
+}
+
+const worldTimestamp = async (timezone: string = 'Etc/GMT'): Promise<WorldTimestamp> => {
     const res: Response = await fetch(`https://worldtimeapi.org/api/timezone/${timezone}`);
-    const json: any = await res.json();
+    const json: DateTimeJsonResponse = await res.json();
 
     const status = res.status;
 
     if (status >= 400)
-        throw Error(json.message);
+        throw Error(res.statusText);
 
-    const datetime: string = json.datetime;
+    const utcDatetime = json.utc_datetime;
 
-    const microseconds = +datetime.match(/\.\d{3}(\d*?)\+/)![1];
-    const serverTimestamp = new Date(datetime).getTime() + microseconds / 1000;
+    const microseconds = +utcDatetime.match(/\.\d{3}(\d*?)\+/)![1];
+    const utcTimestamp = new Date(utcDatetime).getTime() + microseconds / 1000;
+    const timestamp = utcTimestamp + json.raw_offset * 1000;
 
-    return serverTimestamp;
+    return {
+        response: json,
+        timestamp: timestamp,
+        utcTimestamp: utcTimestamp,
+    };
 }
 
-export default getWorldTimestamp;
+export default worldTimestamp;
